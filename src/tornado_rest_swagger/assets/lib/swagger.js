d0719511fa650882d23b89fb0cf95ffa4802e260
Victor Sergienko <singalen@gmail.com>
2014-03-25 18:49:51 +0200

    Better Swagger API titles

--- src/tornado_rest_swagger/assets/lib/swagger.js
+++ src/tornado_rest_swagger/assets/lib/swagger.js
@@ -268,7 +268,7 @@
     SwaggerResource.prototype.consumes = null;
 
     function SwaggerResource(resourceObj, api) {
-      var consumes, e, obj, parts, produces,
+      var consumes, e, obj, produces,
         _this = this;
       this.api = api;
       this.api = this.api;
@@ -276,8 +276,7 @@
       consumes = [];
       this.path = this.api.resourcePath != null ? this.api.resourcePath : resourceObj.path;
       this.description = resourceObj.description;
-      parts = this.path.split("/");
-      this.name = parts[parts.length - 1].replace('.{format}', '');
+      this.name = this.path.replace('.{format}', '');
       this.basePath = this.api.basePath;
       this.operations = {};
       this.operationsArray = [];
